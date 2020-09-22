d0902a7159e907fea68d5ae116a8f6642f7894e4
Thais <thaiscalazans02@gmail.com>
2020-09-22 11:17:00 -0300

    NodeJS comentado para estudo

--- Backend/config.js
+++ Backend/config.js
@@ -2,43 +2,50 @@ var context = document.getElementById("chart").getContext("2d");
 var configuration = {
     type: 'line',
     // Estilização das linhas
+    // Data = Dados gerais
     data: {
+        //Dataset = Conjunto de dados, que nesse caso engloba as linhas e as legendas.
         datasets: [{
-                label: "Temperatura (machos)",
-                borderColor: '#1EC1C4',
-                fill: false,
-                borderWidth: 4,
-                backgroundColor: '#1EC1C4',
-                fontColor: 'White', 
+                //Legenda 1 
+                label: "Temperatura (machos)", //Nome da legenda
+                fontColor: 'White', // Cor da letra da legenda
+                borderColor: '#1EC1C4', // Cor da linha
+                backgroundColor: '#1EC1C4', // Muda a cor do fundo do quadrado da legenda
+                fill: false, // A linha fica sem um fundo, sem uma cor embaixo dela
+                borderWidth: 4, //Espessura da linha do gráfico 
+                
             },
-
+            //Legenda 2
             {
-                label: "Temperatura (fêmeas)",
-                borderColor: '#8ECA18',
-                fill: false,
-                borderWidth: 4,
-                backgroundColor: '#8ECA18',
-                fontColor: 'White',
+                label: "Temperatura (fêmeas)", //Nome da legenda
+                fontColor: 'White', //Cor da fonte 
+                borderColor: '#8ECA18', //Cor da borda tanto da linha quanto do quadrado da legenda 
+                backgroundColor: '#8ECA18', //Cor do fundo do quadrado da legenda e do fundo das bolinhas
+                fill: false, //Sem fundo
+                borderWidth: 4, //Expessura da linha 
             }
         ]
     },
     // Estilizações que não são padrão, "um plus"
     options: {
+        // Estilização do título
         title: {
             text: 'Análise de dados (Temperatura)',
-            fullWidth: true,
-            display: true,
-            position: 'top',
-            fontColor: '#FFFFFF',
-            fontSize: 21,
+            fullWidth: true, //Pega todo o tamanho da 'div' que ele está 
+            display: true,  //Aqui define se o título irá aparecer ou não
+            position: 'top', //Posição do titulo
+            fontColor: '#FFFFFF', // Cor da fonte
+            fontSize: 21, //Tamanho da fonte
         },
 
+        //Configurações a mais da legenda
         legend: {
-            display: 'true',
-            position: 'right',
+            display: 'true', //Se ele vai aparecer
+            position: 'right', //Posição dele
+            
             labels: {
-                fontColor: '#FFFFFF',
-                boxWidth: 50,
+                fontColor: '#FFFFFF', //Cor das fontes
+                boxWidth: 50, 
             }
         },
         scales: {
@@ -46,36 +53,39 @@ var configuration = {
             xAxes: [{
                 //type: 'value',
                 distribution: 'series',
+            // números do lado do eixo x 
                 ticks: {
-                    fontColor: '#FFFFFF',
-                    beginAtZero: true,
-                    label: 'Teste',
+                    fontColor: '#FFFFFF', //cor da fonte 
+                    beginAtZero: true, //deve-se começar do 0? Sim!
                 },
+                //texto no eixo x
                 scaleLabel: {
-                    display: true,
-                    fontColor: 'white',
-                    labelString: 'Tempo',
-                    fontSize: 15,
+                    display: true, //Deve aparecer? Sim!
+                    fontColor: 'white', //Cor da fonte 
+                    labelString: 'Tempo', //texto 
+                    fontSize: 15, //tamanho da fonte 
                 },
             }],
             /* Estilização do Eixo Y*/
             yAxes: [{
                 gridLines: true,
+               // números do lado do eixo y
                 ticks: {
-                    fontColor: '#FFFFFF',
-                    max: 40,
-                    beginAtZero:true,
+                    fontColor: '#FFFFFF', // cor da fonte 
+                    max: 35, //tamanho da fonte 
+                    beginAtZero: true, //aqui indica que os números vão começar do 0
                 },
+                //escrita do lado dos números 
                 scaleLabel: {
-                    display: true,
-                    fontColor: 'white',
-                    labelString: 'Temperatura',
-                    fontSize: 15,
+                    display: true, //Aqui indica se o texto deve aparecer ou não, 'true' representa sim
+                    fontColor: 'white', //Cor da fonte
+                    labelString: 'Temperatura', //texto que será mostrado
+                    fontSize: 15, //tamanho da fonte
                 },
             }]
         },
         animation: {
-            duration: 0
+            duration: 0 //animação de entrada das linhas dos gráficos
         }
     }
 };
@@ -120,30 +130,34 @@ function get_data() {
 
         chart.data.labels.push(this.time++);
         /*
-        Essa parte coloca os dados no gráfico.
-        O +8 do lado do 2 chart significa que o segundo está intercalando 8 números
-        a mais do que o primeiro.
-        Sendo: chart.data.datasets[0].data.push(parseFloat(data)); Os dados da 
-        primeira linha
-        E
-        chart.data.datasets[1].data.push(parseFloat(data)+2.0); Os da segunda.
-        */
+            Vamos começar com palavra por palavra:
+            O 'var n_aleatorios' é uma váriavel que calcula um valor 
+            específico que será usado mais pra frente
+            o 'Math' é um objeto embutido que tem propriedades e funções matemáticas.
+            o 'Random' é uma dessas propriedades/funções matemáticas que são disponibilizadas pelo Math.
+            
+            Nesse caso, estamos fazendo a equação feita no 'sensors.js', para trabalharmos os 'limites da 2a linha' 
+            que é essa abaixo:
+            Math.random() * (max - min) + min;
+            
+            Em que o número 8 é o número máximo que essa segunda linha pode chegar de diferença em comparação a primeira, 
+            sem ter nenhum risco de superar o limite de 33 graus.
+            E o número 7 é o número mínimo que essa 2ª linha pode chegar de diferença em comparação a 1ª, também pra
+            não ficar abaixo do limite de 30 graus, não ficar 29.alguma coisa sabe? 
+            */
         var n_aleatorios = Math.random() * (8 - 7) + 7;
-        /* Essa variável n_aleatorios roda (max-min) + max gerando números
-        aleatórios que criem uma forma mais diferenciada entre os gráficos, intercalando 
-        entre os números possíveis entre os dois valores */
         chart.data.datasets[0].data.push(parseFloat(data));
         chart.data.datasets[1].data.push(parseFloat(data) + n_aleatorios);
         chart.update();
     });
-    // Esses dois comentários não são necessários no gráfico, então não precisa mexer nela.
+    // Esses dois comentários não são necessários no gráfico, então não precisa mexer neles.
     // document.getElementById('average').textContent = obj.average;
     // document.getElementById('averageHour').textContent = obj.averageHour;
 }
 
 get_data();
 // Intervalo entre as atualizações de dados
-// Setei para 10 segundos
+// Setei para 5 segundos
 setInterval(() => {
     get_data();
 }, 5000);
