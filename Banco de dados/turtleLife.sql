74e82c7ec1b4657437226370e761e4b7bd499a42
Karla Moncayo <karla.moncayo@bandtec.com.br>
2020-09-25 09:56:36 -0300

    atualização

--- Banco de dados/turtleLife.sql
+++ Banco de dados/turtleLife.sql
@@ -1,28 +1,6 @@
 create database turtleLife;
 use turtleLife;
-
-create table dadosPf (
-idDadosPf int primary key auto_increment,
-emailPf varchar(100),
-nomePf varchar(50),
-telefonePf int,
-enderecoPf varchar(60),
-senhaPf varchar(50),
-idade int,
-cpf varchar(40)
-);
-
-insert into dadosPf (emailPf, nomePf, telefonePf, enderecoPf, senhaPf, idade, cpf) values 
-	('lais.aranega@bandtec.com.br', 'lais tejon', 12345678, 'Rua 1', 'tartaruga1234', 19, '8462651982'),
-	('caique.rodrigues@bandtec.com.br', 'caique rodrigues', 23456789, 'Rua 2', 'ursinhoted', 21, '893527500'),
-	('thais.sousa@bandtec.com.br', 'thaís calazans', 34567890, 'Rua 3', 'mavis', 19, '8765437691'), 
-	('felipe.mazurkieviz@bandtec.com.br', 'felipe mallasen', 13579246 , 'Rua 4', 'fut4526', 21, '8763245169'), 
-	('beatriz.santos@bandtec.com.br', 'beatriz santos', 37481935 , 'Rua 5', 'barbie123', 19, '6754359862'), 
-	('karla.moncayo@bandtec.com.br', 'karla moncayo', 93748104, 'Rua 6', 'flor45', 26, '765384952'),
-	('andre.santos@bandtec.com.br', 'andre santos', 92750164, 'Rua 7', 'basquete65', 18, '7828328687'),
-	('christian.almeida@bandtec.com.br','christian almeida', 83926013, 'Rua 8', 'skat67', 20, '97864209');
-                      
-                     					
+                                         					
 create table dadosPj (
 idDadosPj int primary key auto_increment,
 nomeEmpresa varchar(50),
@@ -34,8 +12,15 @@ tipoEmpresa varchar(60),
 cnpj int(40)
 );
 
-insert into dadosPj (nomeEmpresa, emailPj, telefonePj, enderecoPj, senhaPj, tipoEmpresa, cnpj) values 
-	();
+insert into dadosPJ (nomeEmpresa, emailPJ, telefonePJ, enderecoPJ, senhaPJ, tipoEmpresa, cnpj) values 
+	('Tejon','lais.aranega@bandtec.com.br', 12345678, 'Rua 1', 'tartaruga1234', 'Pesca', '8462651982'),
+	('Rodrigues','caique.rodrigues@bandtec.com.br', 23456789, 'Rua 2', 'ursinhoted', 'Ambiental', '893527500'),
+	('Calazans','thais.sousa@bandtec.com.br', 34567890, 'Rua 3', 'mavis', 'Pesca', '8765437691'), 
+	('Mallasen','felipe.mazurkieviz@bandtec.com.br', 13579246 , 'Rua 4', 'fut4526', 'Ambiental', '8763245169'), 
+	('Santos','beatriz.santos@bandtec.com.br', 37481935 , 'Rua 5', 'barbie123', 'Pesca', '6754359862'), 
+	('Moncayo','karla.moncayo@bandtec.com.br', 93748104, 'Rua 6', 'flor45', 'Ambiental', '765384952'),
+	('Dedé','andre.santos@bandtec.com.br', 92750164, 'Rua 7', 'basquete65', 'Pesca', '7828328687'),
+	('Almeida','christian.almeida@bandtec.com.br', 83926013, 'Rua 8', 'skat67', 'Ambiental', '97864209');
 
 create table pagamentos (
 idPagamentos int primary key auto_increment,
@@ -50,8 +35,6 @@ cvcCartao int,
 constraint fk_pagamentos_dadosPj foreign key (idDadosPj) references dadosPj (idDadosPj),
 idDadosPj int
 );
-
-
     
 create table periodoInc (
 idCiclo int primary key,
@@ -70,16 +53,17 @@ insert into periodoInc values
 
 create table cliente (
 numPedido int primary key auto_increment,
-idCliente int, 
+idCliente int,
 nomeCliente varchar(40),
+sensores varchar(20), 
 sexoPreferencia varchar(40),
 constraint fk_cliente_periodoInc  FOREIGN KEY (idCiclo) REFERENCES periodoInc (idCiclo),
 idCiclo int
  );
  
 insert into cliente (idCliente,nomeCliente,sexoPreferencia,idCiclo) values
-	(111, 'Projeto tamar', 'macho', 1),
-	(111, 'projeto tamar','femea', 2); 
+	(111, 'Projeto tamar','Sensor 1', 'macho', 1),
+	(111, 'projeto tamar','Sensor 2', 'femea', 2); 
 
 
 create table incubacao(
@@ -89,10 +73,10 @@ matriz varchar(20),
 nOvos int,
 dataInicio varchar(20),
 dataLimite varchar(20),
-numPedido int,
-idCiclo int,
 constraint fk_incubacao_periodoInc FOREIGN KEY (idCiclo) REFERENCES periodoInc (idCiclo),
-constraint fk_incubacao_cliente FOREIGN KEY (numPedido) REFERENCES cliente (numPedido) 
+constraint fk_incubacao_cliente FOREIGN KEY (numPedido) REFERENCES cliente (numPedido),
+numPedido int,
+idCiclo int 
 );
 
 insert into incubacao (descIncubacao, matriz, nOvos, dataInicio, dataLimite, numPedido, idCiclo) 
