f8b19019855f1aeab262380ccbd535ece57d73bd
lahtejon <lais.tejon@hotmail.com>
2020-10-20 12:35:54 -0300

    relatorio dos alertas

--- Banco de dados/turtleLife.sql
+++ Banco de dados/turtleLife.sql
@@ -1,18 +1,18 @@
 create database turtleLife;
 use turtleLife;
                                          					
-create table dadosPj (
-idDadosPj int primary key auto_increment,
+create table cadastro (
+idcadastro int primary key auto_increment,
 nomeEmpresa varchar(50),
-emailPj varchar(100),
-telefonePj int,
-enderecoPj varchar(60),
-senhaPj varchar(50),
+email varchar(100),
+telefone int,
+endereco varchar(60),
+senha varchar(50),
 tipoEmpresa varchar(60),
 cnpj varchar(20)
 );
 
-insert into dadosPJ (nomeEmpresa, emailPJ, telefonePJ, enderecoPJ, senhaPJ, tipoEmpresa, cnpj) values 
+insert into dados (nomeEmpresa, email, telefone, endereco, senha, tipoEmpresa, cnpj) values 
 	('Tejon','lais.aranega@bandtec.com.br', 12345678, 'Rua 1', 'tartaruga1234', 'Pesca', '8462651982'),
 	('Rodrigues','caique.rodrigues@bandtec.com.br', 23456789, 'Rua 2', 'ursinhoted', 'Ambiental', '893527500'),
 	('Amancio','leonardo.asilva@bandtec.com.br', 34567890, 'Rua 3', 'tec1234', 'Pesca', '8765437691'), 
@@ -30,13 +30,14 @@ nomeCartao varchar(50),
 cnpjCartao varchar(40),
 validadeCartao date,
 cvcCartao int,
-constraint fk_pagamentos_dadosPj foreign key (idDadosPj) references dadosPj (idDadosPj),
-idDadosPj int
+fk_idcadastro int, 
+foreign key (fk_idcadastro) references cadastro (idcadastro)
+
 );
     
-create table periodoInc (
+create table Ciclo (
 idCiclo int primary key,
-descPeriodo varchar(40),
+descricao varchar(40),
 primeiroP_dias varchar(40),
 primeioroP_temp float,
 segundoP_dias varchar(40), 
@@ -49,46 +50,48 @@ insert into periodoInc values
 	(1,'macho','20 dias', 24,'20 dias',24,'até 20 dias','temperatura ambiente'),
 	(2,'femea','20 dias', 31 ,'20 dias',31,'até 20 dias','temperatura ambiente');
 
-create table cliente (
-numPedido int primary key auto_increment,
-idCliente int,
-nomeCliente varchar(40),
-sexoPreferencia varchar(10),
-constraint fk_cliente_periodoInc  FOREIGN KEY (idCiclo) REFERENCES periodoInc (idCiclo),
-idCiclo int
+create table pedido (
+idPedido int primary key auto_increment,
+fk_idcadastro int,
+foreign key ( fk_idcadastro)  REFERENCES cadastro (idCadastro),
+fk_idciclo int, 
+foreign key ( fk_idciclo)  REFERENCES ciclo (idCiclo)
+   
  );
 
-insert into cliente (idCliente,nomeCliente,sexoPreferencia,idCiclo) values
-	(111, 'Projeto tamar','macho', 1),
-	(111, 'projeto tamar','femea', 2); 
+insert into pedido  (fk_idcadastro,fk_idciclo) values
+	(1,1),
+	(2,2); 
 
+create table sensor(
+ idsensor int primary key auto_increment,
+ incubadora varchar (40)
+ );
+ 
+insert into sensor (incubadora) value('001m');
+				
 
+							
 create table incubacao(
 idIncubacao int primary key auto_increment,
-descIncubacao varchar(40),
 matriz varchar(20),
 nOvos int,
 dataInicio date,
 dataLimite date,
-constraint fk_incubacao_periodoInc FOREIGN KEY (idCiclo) REFERENCES periodoInc (idCiclo),
-constraint fk_incubacao_cliente FOREIGN KEY (numPedido) REFERENCES cliente (numPedido),
-numPedido int,
-idCiclo int 
+fk_idpedido int, FOREIGN KEY (fk_idpedido) REFERENCES pedido (idpedido),
+fk_idsensor int, FOREIGN KEY (fk_idsensor) REFERENCES sensor (idsensor)
 );
 
 insert into incubacao (descIncubacao, matriz, nOvos, dataInicio, dataLimite, numPedido, idCiclo) 
 values 
-                             ('1º incubação de 2020','amélia',120,'2020-04-10','2020-10-06',1,2),
-							 ('2º incubação de 2020', 'luara',120, '2020-04-10','2020-10-06',2,1);
+                             ('amélia',120,'2020-04-10','2020-10-06',1,2),
+							 ('luara',120, '2020-04-10','2020-10-06',2,1);
                              
+create table leitura_dados(
+idleitura int primary key auto_increment,
+temperatura float,
+data_horario datetime,
+fk_idsensor	int, FOREIGN KEY (fk_idsensor) REFERENCES sensor (idsensor)			
                              
- create table sensor(
- idsensor int primary key auto_increment,
- temperatura float,
- constraint Fk_sensor_incubacao FOREIGN KEy (idIncubacao) REFERENCES incubacao (idIncubacao),
- idIncubacao int);
-
-insert into sensor (temperatura,idIncubacao) value
-
-                                 (33,1),
-								 (24,2); 
+);                             
+ 
